a840af812baee23cb01a17544581fc8611b72918
ikwerre-dev <investorhonour@gmail.com>
2024-10-12 06:49:46 +0100

    updated login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,8 +29,8 @@ const Login = () => {
                     </div>
                     <Link to={'/'}>
                         <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Login
-                        </button>
+                             Login
+                        </button> 
                     </Link>
                 </form>
                 <div className="mt-6 text-center">
